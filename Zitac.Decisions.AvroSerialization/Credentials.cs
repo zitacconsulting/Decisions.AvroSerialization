723402888cef741fe8f2cba40177842961199b13
LinuZitac <linus.appelgren@zitac.se>
2023-03-10 17:52:27 +0100

    Total Rebuild

---
+++ Zitac.Decisions.AvroSerialization/Credentials.cs
@@ -0,0 +1,25 @@
+﻿using DecisionsFramework.Design.ConfigurationStorage.Attributes;
+using DecisionsFramework.Design.Flow.Service.Debugging.DebugData;
+using DecisionsFramework.Design.Properties.Attributes;
+
+namespace Zitac.Decisions.AvroSerialization
+{
+    internal class Credentials : IDebuggerJsonProvider
+    {
+        [WritableValue]
+        public string Username { get; set; }
+
+        [WritableValue]
+        [PasswordText]
+        public string Password { get; set; }
+
+        public object GetJsonDebugView()
+        {
+            return new
+            {
+                Username = this.Username,
+                Password = "********"
+            };
+        }
+    }
+}
---
+++ Zitac.Decisions.AvroSerialization/DeserializeMessage.cs
@@ -0,0 +1,105 @@
+﻿using DecisionsFramework.Design.Flow;
+
+using Avro.Generic;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+using DecisionsFramework.Design.Flow.CoreSteps;
+using DecisionsFramework.Design.Flow.Mapping;
+
+namespace Zitac.Decisions.AvroSerialization
+{
+    [AutoRegisterStep("Deserialize Message", "Integration", "Message Queues", "Zitac")]
+    public class DeserializeMessage : BaseFlowAwareStep, ISyncStep, IDataConsumer, IDataProducer
+    {
+        public DataDescription[] InputData
+        {
+            get
+            {
+                List<DataDescription> inputList = new List<DataDescription>();
+                inputList.Add(new DataDescription((DecisionsType)new DecisionsNativeType(typeof(byte)), "Byte Array Message", true, false, false));
+                inputList.Add(new DataDescription((DecisionsType)new DecisionsNativeType(typeof(Object)), "Registry Connection"));
+                return inputList.ToArray();
+            }
+        }
+
+        public override OutcomeScenarioData[] OutcomeScenarios
+        {
+            get
+            {
+                return new[] {
+                    new OutcomeScenarioData("Done", new DataDescription(typeof(string), "JSON Message")),
+                    new OutcomeScenarioData("Error")
+                };
+            }
+        }
+
+        public ResultData Run(StepStartData data)
+        {
+            try
+            {
+                Object connector = data["Registry Connection"] as Object;
+                byte[] message = data["Byte Array Message"] as byte[];
+
+                IDeserializer<GenericRecord> deserializer = (IDeserializer<GenericRecord>)connector;
+                var deserializedMessage = deserializer.Deserialize(message, false, new SerializationContext());
+                var contents = ExtractContents(deserializedMessage);
+
+                Dictionary<string, object> dictionary = new Dictionary<string, object>();
+                dictionary.Add("JSON Message", (string)JsonConvert.SerializeObject(contents));
+
+                return new ResultData("Done", (IDictionary<string, object>)dictionary);
+
+            }
+            catch(Exception e)
+            {
+                return new ResultData("Error", (IDictionary<string, object>)new Dictionary<string, object>()
+                {
+                {
+                    "Error Message",
+                    (object) e.Message
+                }
+                });
+            }
+        }
+
+        private static object ExtractContents(object obj)
+        {
+            if (obj is GenericRecord record)
+            {
+                var contents = new Dictionary<string, object>();
+
+                foreach (var field in record.Schema.Fields)
+                {
+                    var value = ExtractContents(record[field.Name]);
+
+                    if (value != null)
+                    {
+                        contents[field.Name] = value;
+                    }
+                }
+
+                return contents;
+            }
+            else if (obj is ICollection<object> collection)
+            {
+                var contents = new List<object>();
+
+                foreach (var item in collection)
+                {
+                    var value = ExtractContents(item);
+
+                    if (value != null)
+                    {
+                        contents.Add(value);
+                    }
+                }
+
+                return contents;
+            }
+            else
+            {
+                return obj;
+            }
+        }
+    }
+}
